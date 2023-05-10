40d605a959a03992afe50ead827ecc1fc43baed4
RuslanDidun <ruslandidun5@gmail.com>
2023-05-10 06:15:26 -0600

    promptCard, prompts api

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,59 @@
+'use client'
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
+
+
+  const fetchPosts = async () => {
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
+    setAllPosts(data);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+
+  }
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or username'
+          value={searchText}
+          onChange={handleSearchChange}
+          className='search_input peer'
+          required
+        />
+      </form>
+
+      <PromptCardList
+        data={allPosts}
+        handleTagClick={() => { }}
+      />
+
+    </section>
   )
 }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,60 @@
+'use client'
 
-const PromptCard = () => {
+import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import Image from "next/image";
+
+
+
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
-    <div>PromptCard</div>
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+          <Image
+            className='rounded-full object-contain'
+            src={post.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+          />
+          <div>
+            <h3 className='font-satoshi font-semibold text-gray-900'>
+              {post.creator.username}
+            </h3>
+            <p className='font-inter text-sm text-gray-500'>
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className='copy_btn' onClick={handleCopy}>
+          <Image src={copied === post.prompt
+            ? "/assets/icons/tick.svg"
+            : "/assets/icons/copy.svg"}
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p className='font-inter text-sm blue_gradient cursor-pointer'
+        //ensure that we have a tag
+        onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}
+      </p>
+    </div>
   )
 }
 
-export default PromptCard
+export default PromptCard;
