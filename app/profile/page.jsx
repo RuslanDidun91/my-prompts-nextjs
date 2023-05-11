764d4d4f65ca0ae96e86d14e9ce5db98a3fc2470
RuslanDidun <ruslandidun5@gmail.com>
2023-05-10 19:39:31 -0600

    profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,43 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+
+const MyProfile = () => {
+
+  const {data: session} = useSession();
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async () => {
+
+  }
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your profile page'
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  )
+}
+
+export default MyProfile;
